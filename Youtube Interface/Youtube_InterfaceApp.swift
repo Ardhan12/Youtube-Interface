32db901a5455b508be96d6f857ad6658fa8fd2c4
Ardhan12 <arief.ramadhan43@gmail.com>
2022-07-27 08:38:51 +0700

    Initial Commit

---
+++ Youtube Interface/Youtube_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_InterfaceApp.swift
+//  Youtube Interface
+//
+//  Created by Arief Ramadhan on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
